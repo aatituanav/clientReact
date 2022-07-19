2993a153e5e1d268d239da916244bb015dbac976
Andres Tituana <alvaro.andres1996@hotmail.com>
2022-07-18 20:48:55 -0500

    final revision

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { FaUserAlt } from 'react-icons/fa';
+
 
 const Card = ({ user }) => {
+
     return (
         <div className="w5 h-50 bg-light-green br3 pa3 ma2 grow bw2 shadow-5 tc flex flex-column">
             <div className="h-50">
-                <img className="h-100 br3" alt='photo' src={user["avatar"]} />
+                {user["avatar"] === undefined ? <FaUserAlt className="h-100 f-5-ns"/> :
+                    <img className="h-100 br3" alt='person' src={user["avatar"]} />
+                }
+
             </div>
             <div className="h-50">
                 <h2>{user["name"]}</h2>
                 <p>{user["email"]}</p>
                 <p>{user["position"]}</p>
             </div>
-        </div> 
+        </div>
     );
 }
 
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,24 +1,28 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from '../containers/cardlist.js'
-
-const users = require('../services/users.json');
+import ReactLoading from 'react-loading';
 
 const App = () => {
 
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:4000/users')
             .then(Response => Response.json())
             .then(userss => {
-                console.log(users)
                 setUsers(userss)
             })
     }, [])
     return (
         <div className="tc">
             <h1 className="f1">Usuarios</h1>
-            <CardList users={users} />
+            {users == null ?
+                <div className="flex justify-center">
+                    <ReactLoading type="bubbles" color="red" height={667} width={375} />
+                </div>
+                :
+                <CardList users={users} />
+            }
         </div>
     )
 }
